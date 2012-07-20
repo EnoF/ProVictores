af52cc1acf00c04c6e38e5ab773def342b6fa8fc
Andy Tang <andy@provictores.com>
2012-07-20 12:45:25 +0200

    Test Setup of ProVictores created

---
+++ test/specs/guildmembers-spec.js
@@ -0,0 +1,19 @@
+describe('A guest visits the guild page to apply for our guild', function(){
+	
+	it('should be able to show a list of all guild members', function(){
+		//Given The Guest is on the Home Screen
+		
+		//When The Guest clicks on the Guld Tab
+		
+		//Then The Guest should see a list of Guild Members
+	});
+	
+	it('should be able to look through our member list to see the details of a guild member', function(){
+		//Given The Guest is on the Guild Tab
+		
+		//When The Guest clicks on a Guild member
+		
+		//Then The Guest should see the UserName, Rank and Games of the Member
+	});
+	
+});

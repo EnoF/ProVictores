2f9e8bf95442388a7585426c04d4cde29650da09
Andy Tang <andy@provictores.com>
2012-07-24 21:22:47 +0200

    page loader spec adjusted

---
+++ README.md
@@ -0,0 +1,15 @@
+ProVictores
+=====
+This is the guild website of ProVictores
+
+Jasmine
+-----
+In this project Jasmine is choosen as BDD framework. All specs will be defined, tested and developed with Jasmine.
+
+Widgets
+-----
+Instead of creating pages, this website will be creating widgets as pages. This allows the website to reuse a webpage as component inside a different container. 
+
+Goal
+-----
+The goal is to experience BDD, Github and create a guild website for ProVictores
--- test/specs/core/core-javascript/util/pageloader.js
+++ test/specs/core/core-javascript/util/pageloader.js
@@ -22,20 +22,24 @@ describe('module : pageloader', function(){
 			_html;
 		expect(pageloader).toBeDefined();
 		
-		//When
+		//When Loading the widget page Home
 		pageloader.loadPage('home').then(function(html){
 			_loaded = true;
-			_html = html;
+			_html = $("<html>").html(html);
 		});
 		
 		waitsFor(function(){
 			return _loaded;
 		});
 		
-		//Then
+		//Then The page should be an html page containing
+		//  a h2 with as text Welcome to ProVictores
+		//  a p with as text This is the guild website of ProVictores!
 		runs(function(){
 			expect(_loaded).toBeTruthy();
 			expect(_html.length > 0).toBeTruthy();
+			expect(_html.find("h2").text()).toEqual("Welcome to ProVictores");
+			expect(_html.find("p").text()).toEqual("This is the guild website of ProVictores!");
 		});
 	});
 	

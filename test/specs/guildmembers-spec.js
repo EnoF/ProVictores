68a7e3ec05ab8ecc30de079750b7e5b9a94fa131
Andy Tang <andy@provictores.com>
2012-07-28 11:03:20 +0200

    added requirements for guildmembers widget

--- test/specs/guildmembers-spec.js
+++ test/specs/guildmembers-spec.js
@@ -1,19 +1,53 @@
-// describe('A guest visits the guild page to apply for our guild', function(){
-// 	
-	// it('should be able to show a list of all guild members', function(){
-		// //Given The Guest is on the Home Screen
-// 		
-		// //When The Guest clicks on the Guld Tab
-// 		
-		// //Then The Guest should see a list of Guild Members
-	// });
-// 	
-	// it('should be able to look through our member list to see the details of a guild member', function(){
-		// //Given The Guest is on the Guild Tab
-// 		
-		// //When The Guest clicks on a Guild member
-// 		
-		// //Then The Guest should see the UserName, Rank and Games of the Member
-	// });
-// 	
-// });
+describe('A guest visits the guild page to apply for our guild', function(){
+	
+	var _guildmembersWidget = $("<section>").data("widget", "guildmembers");
+	
+	beforeEach(function(){
+		$('body').append($('<section>').addClass('.testArea'));
+		$('.testArea').append(_guildmembersWidget);
+		loadWidget();
+	});
+	
+	afterEach(function(){
+		$('.testArea').remove();
+	});
+	
+	it('should be able to show a list of all guild members', function(){
+		//Given The Guildmembers widget has a section
+		//  _guildmembersWidget is our section
+		var _list;
+		 
+		//When The Guest opens the widget
+		//  the widget has been loaded in the before each
+		_list = _guildmembersWidget.find('ul');
+		
+		//Then The Guest should see a list of Guild Members
+		expect(_list.find('li').lenght > 0).toBeTruthy();
+	});
+	
+	it('should be able to look through our member list to see the details of a guild member', function(){
+		//Given The widget contains a list of members
+		var _list = _guildmembersWidget.find('ul');
+		
+		//When The Guest clicks on a Guild member
+		_list.find('li:first').click();
+		
+		//Then The Guest should see the Guild Member widget
+		expect(_guildmembersWidget.find('.pv-guildmember').length > 0).toBeTruthy();
+	});
+	
+	function loadWidget(){
+		
+		var _loaded = false;
+		
+		require(['util/loader'], function(){
+			_guildmembersWidget.widget();
+			_loaded = true;
+		});
+		
+		waitsFor(function(){
+			return _loaded;
+		});
+		
+	}
+});

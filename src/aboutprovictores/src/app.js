eacd13785316d1f1ceeabcc8d34f5fba8951fb18
Andy Tang <andy@provictores.com>
2012-07-30 21:21:09 +0200

    about ProVictores widget created

---
+++ src/aboutprovictores/src/app.js
@@ -0,0 +1,13 @@
+define(['util/loader', 'util/service'],
+function(loader, service){
+	var _private = {
+			
+		},
+		_public = {
+			initialize : function(widget){
+				return $.Deferred().resolve();
+			}
+		};
+	
+	return _public;
+});
---
+++ test/specs/aboutprovictores-spec.js
@@ -0,0 +1,45 @@
+describe('A guest opens about ProVictores widget to gain more information about ProVictores', function(){
+	
+	var _aboutWidget = $("<section>");
+	
+	beforeEach(function(){
+		$('body').append($('<section>').addClass('.testArea'));
+		$('.testArea').append(_aboutWidget);
+	});
+	
+	afterEach(function(){
+		_aboutWidget = $("<section>");
+		$('.testArea').remove();
+	});
+	
+	it('should load the widget with information about ProVictores', function(){
+		//Given The about widget is defined
+		_aboutWidget.data("widget", "aboutprovictores");
+			
+		//When The widget is loaded
+		loadWidget();
+		
+		//Then the widget should have a title "About ProVictores"
+		//  a <p> with text 
+		runs(function(){
+			expect(_aboutWidget.find('h2').text()).toEqual('About ProVictores');
+			expect(_aboutWidget.find('p').length).toBeGreaterThan(0);
+		})
+	});
+	
+	function loadWidget(){
+		
+		var _loaded = false;
+		
+		require(['util/loader'], function(){
+			_aboutWidget.widget().then(function(){
+				_loaded = true;
+			});
+		});
+		
+		waitsFor(function(){
+			return _loaded;
+		});
+		
+	}
+});

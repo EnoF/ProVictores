1d16505ccfb8d4a59fdbec91e616262c926d2102
Andy Tang <andy@provictores.com>
2012-08-07 21:01:07 +0200

    guildmembers widget can now be initialized more then once

--- src/guildmembers/src/app.js
+++ src/guildmembers/src/app.js
@@ -1,54 +1,12 @@
-define(['util/loader', 'util/service'],
-function(loader, service){
-	var _private = {
-			widget : null,
-			bindEvents : function(widget){
-				_private.widget.on('click', 'li', function(){
-					if(_private.widget.is('.pv-disabled')) return;
-					var _li = $(this),
-						_member = _li.data('guildmember'),
-						_memberWidget = _li.find('.pv-widget-placeholder');
-					_memberWidget.data('widget', 'guildmember');
-					_memberWidget.data('widgetParams', _member);
-					widget.find('.pv-guildmember').destroy();
-					_li.append(_memberWidget);
-					_memberWidget.widget();
-				});
-			},
-			loadMemberList : function(){
-				var _deferred = $.Deferred();
-				service.get('guildmember').then(function(memberList){
-					loader.loadTemplate('guildmembers', 'guildmember').then(function(template){
-						var _member,
-							_currentMember,
-							_memberEntry;
-						for(_member in memberList){
-							_currentMember = memberList[_member];
-							_memberEntry = template.find("li").clone();
-							_memberEntry.find(".pv-guildmember-name").text(_currentMember.name);
-							_memberEntry.find(".pv-guildmember-avatar").text(_currentMember.avatar);
-							_memberEntry.find(".pv-guildmember-rank").text(_currentMember.rank);
-							_memberEntry.attr('data-guildmember', _currentMember.id);
-							_private.widget.find("ol").append(_memberEntry);
-						}
-						
-						_deferred.resolve();
-					}).fail(function(){
-						_deferred.reject();
-					});
-				}).fail(function(){
-					_deferred.reject();
-				});
-				return _deferred;
-			}
-		},
-		_public = {
-			initialize : function(widget){
-				_private.widget = widget;
-				_private.bindEvents(widget);
-				return _private.loadMemberList();
-			}
-		};
+define(['./guildmembers'],
+function(Guildmembers){
+	
+	var	_public = {
+		initialize : function(widget){
+			var _guildmemberList = new Guildmembers(widget);
+			return _guildmemberList.loadMemberList();
+		}
+	};
 	
 	return _public;
 });
---
+++ src/guildmembers/src/guildmembers.js
@@ -0,0 +1,53 @@
+define(['util/loader', 'util/service'],
+function(loader, service){
+	function Guildmembers(widget){
+		var _private = {
+			widget : widget,
+			bindEvents : function(){
+				_private.widget.on('click', 'li', function(){
+					if(_private.widget.is('.pv-disabled')) return;
+					var _li = $(this),
+						_member = _li.data('guildmember'),
+						_memberWidget = _li.find('.pv-widget-placeholder');
+					_memberWidget.data('widget', 'guildmember');
+					_memberWidget.data('widgetParams', _member);
+					_private.widget.find('.pv-guildmember').destroy();
+					_li.append(_memberWidget);
+					_memberWidget.widget();
+				});
+			},
+			loadMemberList : function(){
+				var _deferred = $.Deferred();
+				service.get('guildmember').then(function(memberList){
+					loader.loadTemplate('guildmembers', 'guildmember').then(function(template){
+						var _member,
+							_currentMember,
+							_memberEntry;
+						for(_member in memberList){
+							_currentMember = memberList[_member];
+							_memberEntry = template.find("li").clone();
+							_memberEntry.find(".pv-guildmember-name").text(_currentMember.name);
+							_memberEntry.find(".pv-guildmember-avatar").text(_currentMember.avatar);
+							_memberEntry.find(".pv-guildmember-rank").text(_currentMember.rank);
+							_memberEntry.attr('data-guildmember', _currentMember.id);
+							_private.widget.find("ol").append(_memberEntry);
+						}
+						
+						_deferred.resolve();
+					}).fail(function(){
+						_deferred.reject();
+					});
+				}).fail(function(){
+					_deferred.reject();
+				});
+				return _deferred;
+			}
+		};
+		
+		this.loadMemberList = _private.loadMemberList;
+		
+		_private.bindEvents();
+	}
+	
+	return Guildmembers;
+});

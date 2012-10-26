3c6547c1d7162ea58e6ce3378965bd047b43137c
Andy Tang <andy@provictores.com>
2012-10-26 17:18:31 +0200

    attempting to revert the changes

--- src/core/core-javascript/util/deferred.js
+++ src/core/core-javascript/util/deferred.js
@@ -2,8 +2,10 @@ define(function(){
 	function Deferred(){
 		var _deferred = $.Deferred(),
 			_originalThen = _deferred.then,
-			_originalFail = _deferred.fail;
-		
+			_originalDone = _deferred.done,
+			_originalFail = _deferred.fail,
+			_originalAlways = _deferred.always;
+
 		function executePromise(promise, params){
 			try{
 				promise = promise || function(){};
@@ -12,33 +14,43 @@ define(function(){
 				$("body").trigger('exception', e);
 			}
 		}
-		
+
 		_deferred.then = function(promise){
 			_originalThen(function(){
 				executePromise(promise, arguments);
 			});
 			return _deferred;
 		};
-		
+
+		_deferred.done = function(promise){
+			_originalDone(function(){
+				executePromise(promise, arguments);
+			});
+			return _deferred;
+		};
+
 		_deferred.fail = function(promise){
 			_originalFail(function(){
 				executePromise(promise, arguments);
 			});
 			return _deferred;
 		};
-		
-		_deferred.subscribe = function(){
-			var _publishers = arguments;
-			
-			$.when.apply($, _publishers).then(function(){
+
+		_deferred.always = function(promise){
+			_originalAlways(function(){
+				executePromise(promise, arguments);
+			});
+			return _deferred;
+		};
+
+		_deferred.subscribe = function(publisher){
+			publisher.then(function(){
 				_deferred.resolve.apply(_deferred, arguments);
 			}).fail(function(){
 				_deferred.reject.apply(_deferred, arguments);
 			});
-			
-			return _deferred;
 		};
-		
+
 		return _deferred;
 	}
 	return Deferred;
--- test/specs/core/core-javascript/util/deferred-spec.js
+++ test/specs/core/core-javascript/util/deferred-spec.js
@@ -1,147 +1,148 @@
 describe('Module deferred', function(){
 	var Deferred,
 		exceptionRaised;
-	
+
 	require(['util/deferred'], function(deferred){
 		Deferred = deferred;
 	});
-	
+
 	beforeEach(function(){
 		exceptionRaised = false;
-		
+
 		waitsFor(function(){
 			return Deferred;
 		});
 	});
-	
+
 	it('The error in the then should get catched', function(){
 		var _deferred = new Deferred();
-		
+
 		registerThen(_deferred);
-		
+
 		registerException();
-		
+
 		_deferred.resolve();
-		
+
 		runs(function(){
 			expect(exceptionRaised.message).toBe("Some error");
 		});
 	});
-	
+
+	it('The error in the done should get catched', function(){
+		var _deferred = new Deferred();
+
+		registerDone(_deferred);
+
+		registerException();
+
+		_deferred.resolve();
+
+		runs(function(){
+			expect(exceptionRaised.message).toBe("Some error");
+		});
+	});
+
+	it('The error in the fail should get catched', function(){
+		var _deferred = new Deferred();
+
+		registerFail(_deferred);
+
+		registerException();
+
+		_deferred.reject();
+
+		runs(function(){
+			expect(exceptionRaised.message).toBe("Some error");
+		});
+	});
+
+	it('The error in the always should get catched', function(){
+		var _deferred = new Deferred();
+
+		registerAlways(_deferred);
+
+		registerException();
+
+		_deferred.resolve();
+
+		runs(function(){
+			expect(exceptionRaised.message).toBe("Some error");
+		});
+	});
+
 	describe('subscription', function(){
-		
+
 		it('should resolve a deferred when the registered deferred is resolved too', function(){
 			var _subscriber = new Deferred(),
 				_publisher = new Deferred();
-			
+
 			_subscriber.subscribe(_publisher);
-			
+
 			_subscriber.then(function(){
 				throw new Error("Some error");
 			});
-			
+
 			registerException();
-			
+
 			_publisher.resolve();
-			
+
 			runs(function(){
 				expect(exceptionRaised.message).toBe("Some error");
 			});
-			
+
 		});
-		
+
 		it('should reject a deferred when the registered deferred is rejected too', function(){
 			var _subscriber = new Deferred(),
 				_publisher = new Deferred();
-			
+
 			_subscriber.subscribe(_publisher);
-			
+
 			_subscriber.fail(function(){
 				throw new Error("Some error");
 			});
-			
+
 			registerException();
-			
+
 			_publisher.reject();
-			
+
 			runs(function(){
 				expect(exceptionRaised.message).toBe("Some error");
 			});
-			
+
 		});
-		
-		it('should be able to subscribe on multiple deferreds', function(){
-			var _subscriber = new Deferred(),
-				_publisher = new Deferred(),
-				_otherPublisher = new Deferred(),
-				_resolved = false,
-				_rejected = false;
-				
-			_subscriber.subscribe(_publisher, _otherPublisher);
-			
-			_subscriber.then(function(){
-				_resolved = true;
-			});
-			
-			_subscriber.fail(function(){
-				_rejected = true;
-			});
-			
-			_publisher.resolve();
-			_otherPublisher.resolve();
-			
-			waitsFor(function(){
-				return _resolved;
-			});
-			
-			runs(function(){
-				expect(_resolved).toBeTruthy();
-				expect(_rejected).toBeFalsy();
-			});
-		});
-		
-		it('should reject a subscribe whenever one of the deferreds fail', function(){
-			var _subscriber = new Deferred(),
-				_publisher = new Deferred(),
-				_otherPublisher = new Deferred(),
-				_resolved = false,
-				_rejected = false;
-				
-			_subscriber.subscribe(_publisher, _otherPublisher);
-			
-			_subscriber.then(function(){
-				_resolved = true;
-			});
-			
-			_subscriber.fail(function(){
-				_rejected = true;
-			});
-			
-			_publisher.resolve();
-			_otherPublisher.reject();
-			
-			waitsFor(function(){
-				return _rejected;
-			});
-			
-			runs(function(){
-				expect(_resolved).toBeFalsy();
-				expect(_rejected).toBeTruthy();
-			});
-		});
-	});
-	
+
+	})
+
 	function registerThen(deferred){
 		deferred.then(function(){
 			throw new Error("Some error");
 		});
 	}
-	
+
+	function registerDone(deferred){
+		deferred.done(function(){
+			throw new Error("Some error");
+		});
+	}
+
+	function registerFail(deferred){
+		deferred.fail(function(){
+			throw new Error("Some error");
+		});
+	}
+
+	function registerAlways(deferred){
+		deferred.always(function(){
+			throw new Error("Some error");
+		});
+	}
+
 	function registerException(){
 		$('body').one('exception', function(event, error){
 			exceptionRaised = error;
 		});
-		
+
 		waitsFor(function(){
 			return exceptionRaised;
 		});
